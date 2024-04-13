7b9880344a781c436462a51359157802fe30b894
ArchinDeepakTV <archindeepakad.ad@gmail.com>
2024-04-13 14:25:06 +0530

    added a bit of security to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,31 @@ const express = require("express");
 var bodyParser = require("body-parser");
 var session = require("express-session");
 var cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
 
 const app = express();
 const PORT = process.env.PORT || 5050;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(limiter);
+app.use(
+  session({
+    // set a custom name for the session cookie
+    name: "_aravind_v_design",
+    // a secure secret key for session encryption
+    secret: "complex_secret_key",
+    // ! NEED TO MAKE A KEY
+    // Additional session configurations...
+  })
+);
 
 const {
   select_data_from_db,
